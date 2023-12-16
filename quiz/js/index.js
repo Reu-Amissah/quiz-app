dea5f023f4d31266a66ccab33a6b2e677121c9e0
Reu-Amissah <reujoyamissah@gmail.com>
2023-12-15 15:38:33 -1200

    init files

---
+++ quiz/js/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const styled_components_1 = __importDefault(require("styled-components"));
+console.log("here's my first ts project");
+console.log("more typescript");
+let myvar = "hello";
+console.log(myvar);
+const Button = styled_components_1.default.button `
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #bf4f74;
+  color: #bf4f74;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
---
+++ quiz/js/index.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+console.log("here's my first ts project");
+console.log("more typescript");
+
+let myvar: string = "hello";
+
+console.log(myvar);
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #bf4f74;
+  color: #bf4f74;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
