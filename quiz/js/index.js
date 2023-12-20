a323e47c9cbadaa29d25b09a60263aca8c200e6b
Reu-Amissah <reujoyamissah@gmail.com>
2023-12-20 16:03:31 +0000

    try again in the evening

--- quiz/js/index.js
+++ quiz/js/index.js
@@ -1,5 +1,4 @@
 "use strict";
-// import { globalMain } from "./global_variables";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -10,31 +9,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 let Questions = { quizzes: [] };
-function fetchData() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const response = yield fetch("./js/data.json");
-        const data = yield response.json();
-        Questions = data;
-        const category = localStorage.getItem("selectedCategory");
-        switch (category) {
-            case "html":
-                console.log(Questions.quizzes[0]);
-                break;
-            case "css":
-                console.log(Questions.quizzes[1]);
-                break;
-            case "javascript":
-                console.log(Questions.quizzes[2]);
-                break;
-            case "accessibility":
-                console.log(Questions.quizzes[3]);
-                break;
-            default:
-                console.log("unknown");
-        }
-    });
-}
-fetchData();
 // dark mode settings here
 document.addEventListener("DOMContentLoaded", () => {
     let darkMode = document.getElementById("dark-mode");
@@ -42,7 +16,9 @@ document.addEventListener("DOMContentLoaded", () => {
     let darkModeIcon = document.getElementById("dark-icon-container");
     let sunIcon = document.getElementById("sun");
     let moonIcon = document.getElementById("moon");
+    // local storage variables called here
     let currentMode = localStorage.getItem("mode") || "light";
+    let currentCategory = localStorage.getItem("selectedCategory");
     let imageUrl = "./assets/images/pattern-background-desktop-dark.svg";
     let sun = "./assets/images/icon-sun-light.svg";
     let moon = "./assets/images/icon-moon-light.svg";
@@ -70,17 +46,39 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
     darkMode === null || darkMode === void 0 ? void 0 : darkMode.addEventListener("click", setDarkMode);
+    //header or title goes here
+    const categoryTitleElement = document.querySelector(".categories-title");
+    if (currentCategory !== null && categoryTitleElement) {
+        categoryTitleElement.innerHTML =
+            currentCategory.charAt(0).toUpperCase() + currentCategory.slice(1);
+    }
+    const questionHeaderIcon = document.getElementById("question-header-icon");
+    // handle icon and icon color here
+    if (localStorage.getItem("selectedCategory") === "html") {
+        questionHeaderIcon === null || questionHeaderIcon === void 0 ? void 0 : questionHeaderIcon.classList.add("orange");
+        questionHeaderIcon.src = "./assets/images/icon-html.svg";
+    }
+    else if (localStorage.getItem("selectedCategory") === "css") {
+        questionHeaderIcon === null || questionHeaderIcon === void 0 ? void 0 : questionHeaderIcon.classList.add("green");
+        questionHeaderIcon.src = "./assets/images/icon-css.svg";
+    }
+    else if (localStorage.getItem("selectedCategory") === "javascript") {
+        questionHeaderIcon === null || questionHeaderIcon === void 0 ? void 0 : questionHeaderIcon.classList.add("blue");
+        questionHeaderIcon.src = "./assets/images/icon-js.svg";
+    }
+    else if (localStorage.getItem("selectedCategory") === "accessibility") {
+        questionHeaderIcon === null || questionHeaderIcon === void 0 ? void 0 : questionHeaderIcon.classList.add("purple");
+        questionHeaderIcon.src = "./assets/images/icon-accessibility.svg";
+    }
 });
 // dark mode logic ENDS here
-// let selectedCategory;
+// when selectedCategory;
 document.querySelectorAll(".category-link").forEach((link) => {
     link.addEventListener("click", function (event) {
         var _a, _b;
         const categoryTarget = event.currentTarget;
         const categoryTitle = (_b = (_a = categoryTarget
             .querySelector(".categories-titles")) === null || _a === void 0 ? void 0 : _a.textContent) === null || _b === void 0 ? void 0 : _b.toLowerCase();
-        // categoryTitle.href
-        // this.href = `./questions.html?category=${categoryTitle}`;
         if (categoryTitle) {
             categoryTarget.href = `./questions.html?category=${categoryTitle}`;
         }
@@ -100,7 +98,66 @@ document.querySelectorAll(".category-link").forEach((link) => {
             default:
                 localStorage.setItem("selectedCategory", "unknown");
         }
-        // You can do more actions here based on the selected category
     });
 });
-console.log(localStorage.getItem("selectedCategory"));
+let score = 0;
+let currentIndexQuestion = 0;
+let filteredCategoryQuestions;
+// localStorage.setItem("currentIndexQuestion", "0");
+// let currentIndexQuestion: string | number | null =
+//   localStorage.getItem("currentIndexQuestion") | 0;
+function fetchData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const response = yield fetch("./js/data.json");
+        const data = yield response.json();
+        Questions = data;
+        const category = localStorage.getItem("selectedCategory");
+        switch (category) {
+            case "html":
+                filteredCategoryQuestions = Questions.quizzes[0];
+                console.log(filteredCategoryQuestions);
+                break;
+            case "css":
+                filteredCategoryQuestions = Questions.quizzes[1];
+                break;
+            case "javascript":
+                filteredCategoryQuestions = Questions.quizzes[2];
+                break;
+            case "accessibility":
+                filteredCategoryQuestions = Questions.quizzes[3];
+                break;
+            default:
+                console.log("unknown");
+        }
+        return filteredCategoryQuestions;
+    });
+}
+fetchData().then(() => {
+    console.log(filteredCategoryQuestions);
+    function startQuiz() {
+        currentIndexQuestion = 0;
+        score = 0;
+        showQuestion(filteredCategoryQuestions);
+        console.log("success");
+    }
+    function showQuestion(questions) {
+        questionBody.innerHTML = questions.questions[0].question;
+        console.log(questionBody === null || questionBody === void 0 ? void 0 : questionBody.innerHTML);
+    }
+    let questionsCategorySelector = document.getElementById("category-selector");
+    questionsCategorySelector === null || questionsCategorySelector === void 0 ? void 0 : questionsCategorySelector.addEventListener("click", startQuiz);
+});
+let questionBody = document.getElementById("question");
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("hi");
+    let button = document.getElementById("button");
+    button === null || button === void 0 ? void 0 : button.addEventListener("click", showNextQuestion);
+    function showNextQuestion() {
+        currentIndexQuestion = currentIndexQuestion + 1;
+    }
+    // let levelNum = 100;
+    // levelNum += 20;
+    // let levelWidth: string = levelNum + "%";
+    // (document.querySelector(".level-indicator") as HTMLElement).style.width =
+    //   levelWidth;
+});
--- quiz/js/index.ts
+++ quiz/js/index.ts
@@ -1,43 +1,16 @@
-// import { globalMain } from "./global_variables";
-
 interface QuestionType {
   quizzes: Array<{
     title: string;
     icon: string;
     questions: Array<{
       question: string;
-      option: Array<string>;
+      options: Array<string>;
       answer: string;
     }>;
   }>;
 }
 
 let Questions: QuestionType = { quizzes: [] };
-
-async function fetchData() {
-  const response = await fetch("./js/data.json");
-  const data = await response.json();
-  Questions = data;
-
-  const category = localStorage.getItem("selectedCategory");
-  switch (category) {
-    case "html":
-      console.log(Questions.quizzes[0]);
-      break;
-    case "css":
-      console.log(Questions.quizzes[1]);
-      break;
-    case "javascript":
-      console.log(Questions.quizzes[2]);
-      break;
-    case "accessibility":
-      console.log(Questions.quizzes[3]);
-      break;
-    default:
-      console.log("unknown");
-  }
-}
-fetchData();
 
 // dark mode settings here
 document.addEventListener("DOMContentLoaded", () => {
@@ -48,7 +21,10 @@ document.addEventListener("DOMContentLoaded", () => {
   let sunIcon = document.getElementById("sun");
   let moonIcon = document.getElementById("moon");
 
+  // local storage variables called here
   let currentMode: string = localStorage.getItem("mode") || "light";
+  let currentCategory: string | null = localStorage.getItem("selectedCategory");
+
   let imageUrl = "./assets/images/pattern-background-desktop-dark.svg";
   let sun = "./assets/images/icon-sun-light.svg";
   let moon = "./assets/images/icon-moon-light.svg";
@@ -78,18 +54,42 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   darkMode?.addEventListener("click", setDarkMode);
+
+  //header or title goes here
+  const categoryTitleElement = document.querySelector(".categories-title");
+  if (currentCategory !== null && categoryTitleElement) {
+    categoryTitleElement.innerHTML =
+      currentCategory.charAt(0).toUpperCase() + currentCategory.slice(1);
+  }
+
+  const questionHeaderIcon = document.getElementById(
+    "question-header-icon"
+  ) as HTMLImageElement;
+
+  // handle icon and icon color here
+  if (localStorage.getItem("selectedCategory") === "html") {
+    questionHeaderIcon?.classList.add("orange");
+    questionHeaderIcon.src = "./assets/images/icon-html.svg";
+  } else if (localStorage.getItem("selectedCategory") === "css") {
+    questionHeaderIcon?.classList.add("green");
+    questionHeaderIcon.src = "./assets/images/icon-css.svg";
+  } else if (localStorage.getItem("selectedCategory") === "javascript") {
+    questionHeaderIcon?.classList.add("blue");
+    questionHeaderIcon.src = "./assets/images/icon-js.svg";
+  } else if (localStorage.getItem("selectedCategory") === "accessibility") {
+    questionHeaderIcon?.classList.add("purple");
+    questionHeaderIcon.src = "./assets/images/icon-accessibility.svg";
+  }
 });
 // dark mode logic ENDS here
 
-// let selectedCategory;
+// when selectedCategory;
 document.querySelectorAll(".category-link").forEach((link) => {
   link.addEventListener("click", function (event) {
     const categoryTarget = event.currentTarget as HTMLAnchorElement;
     const categoryTitle = categoryTarget
       .querySelector(".categories-titles")
       ?.textContent?.toLowerCase();
-    // categoryTitle.href
-    // this.href = `./questions.html?category=${categoryTitle}`;
 
     if (categoryTitle) {
       categoryTarget.href = `./questions.html?category=${categoryTitle}`;
@@ -112,9 +112,84 @@ document.querySelectorAll(".category-link").forEach((link) => {
       default:
         localStorage.setItem("selectedCategory", "unknown");
     }
-
-    // You can do more actions here based on the selected category
   });
 });
 
-console.log(localStorage.getItem("selectedCategory"));
+let score = 0;
+let currentIndexQuestion = 0;
+
+let filteredCategoryQuestions: Quiz;
+// localStorage.setItem("currentIndexQuestion", "0");
+// let currentIndexQuestion: string | number | null =
+//   localStorage.getItem("currentIndexQuestion") | 0;
+
+async function fetchData() {
+  const response = await fetch("./js/data.json");
+  const data = await response.json();
+  Questions = data;
+
+  const category = localStorage.getItem("selectedCategory");
+  switch (category) {
+    case "html":
+      filteredCategoryQuestions = Questions.quizzes[0];
+      console.log(filteredCategoryQuestions);
+      break;
+    case "css":
+      filteredCategoryQuestions = Questions.quizzes[1];
+      break;
+    case "javascript":
+      filteredCategoryQuestions = Questions.quizzes[2];
+      break;
+    case "accessibility":
+      filteredCategoryQuestions = Questions.quizzes[3];
+      break;
+    default:
+      console.log("unknown");
+  }
+
+  return filteredCategoryQuestions;
+}
+
+fetchData().then(() => {
+  console.log(filteredCategoryQuestions);
+  function startQuiz() {
+    currentIndexQuestion = 0;
+    score = 0;
+    showQuestion(filteredCategoryQuestions);
+    console.log("success");
+  }
+  function showQuestion(questions: Quiz) {
+    questionBody!.innerHTML = questions.questions[0].question;
+    console.log(questionBody?.innerHTML);
+  }
+
+  let questionsCategorySelector = document.getElementById("category-selector");
+  questionsCategorySelector?.addEventListener("click", startQuiz);
+});
+
+let questionBody = document.getElementById("question");
+interface Quiz {
+  title: string;
+  icon: string;
+  questions: Array<{
+    question: string;
+    options: Array<string>; // Note that you have 'option' in QuestionType, but 'options' seems more appropriate
+    answer: string;
+  }>;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("hi");
+  let button = document.getElementById("button");
+  button?.addEventListener("click", showNextQuestion);
+
+  function showNextQuestion() {
+    currentIndexQuestion = currentIndexQuestion + 1;
+  }
+
+  // let levelNum = 100;
+  // levelNum += 20;
+  // let levelWidth: string = levelNum + "%";
+  // (document.querySelector(".level-indicator") as HTMLElement).style.width =
+  //   levelWidth;
+});
