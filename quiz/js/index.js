ddc4f6856694673662e0e26d47e1a35234ac0544
Reu-Amissah <reujoyamissah@gmail.com>
2023-12-20 10:21:49 +0000

    feature/ navigation to new page, local storage variables

--- quiz/js/index.js
+++ quiz/js/index.js
@@ -15,6 +15,23 @@ function fetchData() {
         const response = yield fetch("./js/data.json");
         const data = yield response.json();
         Questions = data;
+        const category = localStorage.getItem("selectedCategory");
+        switch (category) {
+            case "html":
+                console.log(Questions.quizzes[0]);
+                break;
+            case "css":
+                console.log(Questions.quizzes[1]);
+                break;
+            case "javascript":
+                console.log(Questions.quizzes[2]);
+                break;
+            case "accessibility":
+                console.log(Questions.quizzes[3]);
+                break;
+            default:
+                console.log("unknown");
+        }
     });
 }
 fetchData();
@@ -23,23 +40,67 @@ document.addEventListener("DOMContentLoaded", () => {
     let darkMode = document.getElementById("dark-mode");
     darkMode === null || darkMode === void 0 ? void 0 : darkMode.addEventListener("click", setDarkMode);
     let darkModeIcon = document.getElementById("dark-icon-container");
-    let lightMode = true;
+    let sunIcon = document.getElementById("sun");
+    let moonIcon = document.getElementById("moon");
+    let currentMode = localStorage.getItem("mode") || "light";
     let imageUrl = "./assets/images/pattern-background-desktop-dark.svg";
+    let sun = "./assets/images/icon-sun-light.svg";
+    let moon = "./assets/images/icon-moon-light.svg";
+    toggleDarkMode(currentMode);
     function setDarkMode() {
-        lightMode = !lightMode;
-        toggleDarkMode();
+        const systemMode = localStorage.getItem("mode") || "light";
+        const newMode = systemMode === "light" ? "dark" : "light";
+        localStorage.setItem("mode", newMode);
+        toggleDarkMode(newMode);
     }
-    function toggleDarkMode() {
-        if (!lightMode) {
+    function toggleDarkMode(mode) {
+        if (mode === "light") {
             darkModeIcon.style.justifyContent = "flex-end";
             document.body.classList.add("dark-theme");
             document.body.style.backgroundImage = `url(${imageUrl})`;
+            sunIcon.style.backgroundImage = `url(${sun})`;
+            moonIcon.style.backgroundImage = `url(${moon})`;
         }
         else {
             darkModeIcon.style.justifyContent = "flex-start";
             document.body.classList.remove("dark-theme");
             document.body.style.backgroundImage = "";
+            sunIcon.style.backgroundImage = "";
+            moonIcon.style.backgroundImage = "";
         }
     }
     darkMode === null || darkMode === void 0 ? void 0 : darkMode.addEventListener("click", setDarkMode);
 });
+// dark mode logic ENDS here
+// let selectedCategory;
+document.querySelectorAll(".category-link").forEach((link) => {
+    link.addEventListener("click", function (event) {
+        var _a, _b;
+        const categoryTarget = event.currentTarget;
+        const categoryTitle = (_b = (_a = categoryTarget
+            .querySelector(".categories-titles")) === null || _a === void 0 ? void 0 : _a.textContent) === null || _b === void 0 ? void 0 : _b.toLowerCase();
+        // categoryTitle.href
+        // this.href = `./questions.html?category=${categoryTitle}`;
+        if (categoryTitle) {
+            categoryTarget.href = `./questions.html?category=${categoryTitle}`;
+        }
+        switch (categoryTitle) {
+            case "html":
+                localStorage.setItem("selectedCategory", "html");
+                break;
+            case "css":
+                localStorage.setItem("selectedCategory", "css");
+                break;
+            case "javascript":
+                localStorage.setItem("selectedCategory", "javascript");
+                break;
+            case "accessibility":
+                localStorage.setItem("selectedCategory", "accessibility");
+                break;
+            default:
+                localStorage.setItem("selectedCategory", "unknown");
+        }
+        // You can do more actions here based on the selected category
+    });
+});
+console.log(localStorage.getItem("selectedCategory"));
--- quiz/js/index.ts
+++ quiz/js/index.ts
@@ -18,6 +18,24 @@ async function fetchData() {
   const response = await fetch("./js/data.json");
   const data = await response.json();
   Questions = data;
+
+  const category = localStorage.getItem("selectedCategory");
+  switch (category) {
+    case "html":
+      console.log(Questions.quizzes[0]);
+      break;
+    case "css":
+      console.log(Questions.quizzes[1]);
+      break;
+    case "javascript":
+      console.log(Questions.quizzes[2]);
+      break;
+    case "accessibility":
+      console.log(Questions.quizzes[3]);
+      break;
+    default:
+      console.log("unknown");
+  }
 }
 fetchData();
 
@@ -27,25 +45,76 @@ document.addEventListener("DOMContentLoaded", () => {
   darkMode?.addEventListener("click", setDarkMode);
 
   let darkModeIcon = document.getElementById("dark-icon-container");
-  let lightMode: boolean = true;
+  let sunIcon = document.getElementById("sun");
+  let moonIcon = document.getElementById("moon");
+
+  let currentMode: string = localStorage.getItem("mode") || "light";
   let imageUrl = "./assets/images/pattern-background-desktop-dark.svg";
+  let sun = "./assets/images/icon-sun-light.svg";
+  let moon = "./assets/images/icon-moon-light.svg";
+  toggleDarkMode(currentMode);
 
   function setDarkMode() {
-    lightMode = !lightMode;
-    toggleDarkMode();
+    const systemMode = localStorage.getItem("mode") || "light";
+    const newMode = systemMode === "light" ? "dark" : "light";
+    localStorage.setItem("mode", newMode);
+    toggleDarkMode(newMode);
   }
 
-  function toggleDarkMode() {
-    if (!lightMode) {
+  function toggleDarkMode(mode: string) {
+    if (mode === "light") {
       darkModeIcon!.style.justifyContent = "flex-end";
       document.body.classList.add("dark-theme");
       document.body.style.backgroundImage = `url(${imageUrl})`;
+      sunIcon!.style.backgroundImage = `url(${sun})`;
+      moonIcon!.style.backgroundImage = `url(${moon})`;
     } else {
       darkModeIcon!.style.justifyContent = "flex-start";
       document.body.classList.remove("dark-theme");
       document.body.style.backgroundImage = "";
+      sunIcon!.style.backgroundImage = "";
+      moonIcon!.style.backgroundImage = "";
     }
   }
 
   darkMode?.addEventListener("click", setDarkMode);
 });
+// dark mode logic ENDS here
+
+// let selectedCategory;
+document.querySelectorAll(".category-link").forEach((link) => {
+  link.addEventListener("click", function (event) {
+    const categoryTarget = event.currentTarget as HTMLAnchorElement;
+    const categoryTitle = categoryTarget
+      .querySelector(".categories-titles")
+      ?.textContent?.toLowerCase();
+    // categoryTitle.href
+    // this.href = `./questions.html?category=${categoryTitle}`;
+
+    if (categoryTitle) {
+      categoryTarget.href = `./questions.html?category=${categoryTitle}`;
+    }
+
+    switch (categoryTitle) {
+      case "html":
+        localStorage.setItem("selectedCategory", "html");
+
+        break;
+      case "css":
+        localStorage.setItem("selectedCategory", "css");
+        break;
+      case "javascript":
+        localStorage.setItem("selectedCategory", "javascript");
+        break;
+      case "accessibility":
+        localStorage.setItem("selectedCategory", "accessibility");
+        break;
+      default:
+        localStorage.setItem("selectedCategory", "unknown");
+    }
+
+    // You can do more actions here based on the selected category
+  });
+});
+
+console.log(localStorage.getItem("selectedCategory"));
